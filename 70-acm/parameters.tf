75a6af181ff41655833be240d4b691fc8c168c5b
chakradhar06 <chakradhar06@gmail.com>
2025-12-29 02:42:33 -0500

    u

--- 70-acm/parameters.tf
+++ 70-acm/parameters.tf
@@ -2,4 +2,8 @@ resource "aws_ssm_parameter" "ingress_alb_certificate_arn" {
   name  = "/${var.project_name}/${var.environment}/ingress_alb_certificate_arn"
   type  = "String"
   value = aws_acm_certificate.roboshop.arn
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
